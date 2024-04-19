0b56af50b231e210669df80134066ff18a59e065
yntedero <yntedero@gmail.com>
2024-04-20 00:36:02 +0200

    Enhanced AutocompleteComponent with dynamic filtering, sorting, and loading state management; added responsive UI updates and comprehensive unit tests to ensure functionality; improved styling for better usability and consistency; updated AppComponent with title for clearer context."

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,6 +1,6 @@
-import {Component, Input} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {NgForOf, NgIf} from "@angular/common";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { NgForOf, NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-autocomplete',
@@ -11,18 +11,33 @@ import {NgForOf, NgIf} from "@angular/common";
     NgIf
   ],
   templateUrl: './autocomplete.component.html',
-  styleUrl: './autocomplete.component.scss'
+  styleUrls: ['./autocomplete.component.scss']
 })
 export class AutocompleteComponent {
-  @Input() options: string[] = []; //
+  @Input() options: string[] = [];
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() filterPredicate: (option: string, searchTerm: string) => boolean = (option, searchTerm) => option.toLowerCase().includes(searchTerm.toLowerCase());
+  @Input() sortPredicate: (a: string, b: string) => number = () => 0;
+
+  @Output() search = new EventEmitter<string>();
+
   searchTerm: string = '';
   filteredOptions: string[] = [];
 
+  title: string = 'Hello, auto-complete-coreteq';
+
   onSearchChange(): void {
-    this.filteredOptions = this.searchTerm ?
-      this.options.filter(option =>
-        option.toLowerCase().includes(this.searchTerm.toLowerCase())
-      ) : [];
+    if (this.disabled || this.loading) return;
+    let options = this.options;
+    if (this.filterPredicate) {
+      options = options.filter(option => this.filterPredicate(option, this.searchTerm));
+    }
+    if (this.sortPredicate) {
+      options.sort(this.sortPredicate);
+    }
+    this.filteredOptions = options;
+    this.search.emit(this.searchTerm);
   }
 
   selectOption(option: string): void {
@@ -30,3 +45,4 @@ export class AutocompleteComponent {
     this.filteredOptions = [];
   }
 }
+
