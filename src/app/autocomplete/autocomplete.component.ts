d4daf876f138c06f9cd71edb0ac43f52bbb03c2c
yntedero <yntedero@gmail.com>
2024-04-19 22:59:51 +0200

    Implemented sorting and filtering methods as needed. Configured two-way data binding with forms or ngModel. Handled events such as opening and closing of the dropdown list, and selection of an option. Added settings for users, if needed, through @Input properties.

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-autocomplete',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './autocomplete.component.html',
   styleUrl: './autocomplete.component.scss'
 })
 export class AutocompleteComponent {
+  @Input() options: string[] = []; //
+  searchTerm: string = '';
+  filteredOptions: string[] = [];
 
+  onSearchChange(): void {
+    this.filteredOptions = this.searchTerm ?
+      this.options.filter(option =>
+        option.toLowerCase().includes(this.searchTerm.toLowerCase())
+      ) : [];
+  }
+
+  selectOption(option: string): void {
+    this.searchTerm = option;
+    this.filteredOptions = [];
+  }
 }
