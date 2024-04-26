2ef9f28f81ec55111844756361f3aced6eb53182
Yurii Ostapchuk <yurii.ostapchuk@student.umb.sk>
2024-04-26 11:56:11 +0200

    deleted autoredirect on autocomplete page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/autocomplete', pathMatch: 'full' },
   { path: 'autocomplete', component: AutocompleteComponent }
 ];
