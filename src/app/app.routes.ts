c8a51b1ec41d328843c36066206c4e323313edb7
yntedero <yntedero@gmail.com>
2024-05-02 00:58:19 +0200

    Changing router to default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { SelectorComponent } from './selector/selector.component';
 
-export const routes: Routes = [
-  { path: 'autocomplete', component: SelectorComponent }
+export const routes: Routes = [ {
+
+}
 ];
